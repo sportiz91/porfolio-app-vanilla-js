6b53995e0e805b2e38fc8b99ce9ebf0666c08024
lasantoneta <sportiz9109@gmail.com>
2021-10-27 15:52:22 -0300

    add projects page before change

--- dist/js/behaviour.js
+++ dist/js/behaviour.js
@@ -2,6 +2,7 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menuBtnBrg = document.querySelector(".menu-btn-burger");
 const nav = document.querySelector(".nav");
+const navUl = document.querySelector(".nav-items-list"); //added
 const listItems = document.querySelectorAll(".nav-item");
 let state = false;
 
@@ -10,11 +11,13 @@ menuBtn.addEventListener("click", () => {
   if (!state) {
     menuBtnBrg.classList.add("open");
     nav.classList.add("open");
+    navUl.classList.add("open"); //added
     listItems.forEach((item) => item.classList.add("open"));
     state = true;
   } else {
     menuBtnBrg.classList.remove("open");
     nav.classList.remove("open");
+    navUl.classList.remove("open"); //added
     listItems.forEach((item) => {
       item.classList.remove("open");
     });
