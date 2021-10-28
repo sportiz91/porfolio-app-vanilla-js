a8bc4dc418abe3ff6b55f5ae3fb9d672aa2aacba
lasantoneta <sportiz9109@gmail.com>
2021-10-28 11:24:39 -0300

    add contact and active class nav

--- dist/js/behaviour.js
+++ dist/js/behaviour.js
@@ -21,3 +21,23 @@ menuBtn.addEventListener("click", () => {
     state = false;
   }
 });
+
+listItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const foundItem = Array.from(listItems).find((item) => {
+      return item.firstElementChild.classList[1] == "active";
+    });
+    foundItem.firstElementChild.classList.remove("active");
+    e.target.classList.add("active");
+  });
+});
+
+/* LEARNINGS --------------------------------------------------------------------------------------------------------------- */
+/* 1. NODELISTS AND ARRAYS: [...nodelist] will make an array of out of an object if the object is iterable.
+Array.from(nodelist) will make an array out of an object if the object is iterable or if the object is array-like (has .length and numeric props)
+our two examples will be identical if NodeList.prototype[Symbol.iterator] exists, because both cases cover iterables. If your environment has 
+not been configured such that NodeList is iterable however, your first example will fail, and the second will succeed. Babel currently does not handle this case properly. 
+So if your NodeList is iterable, it is really up to you which you use. I would likely choose on a case-by-case basis. One benefit of Array.from is 
+that it takes a second argument of a mapping function, whereas the first [...iterable].map(item => item) would have to create a temporary
+array, Array.from(iterable, item => item) would not. If you are not mapping the list however, it does not matter
+*/
